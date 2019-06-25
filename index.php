5b7516d953f758e6ec0e5124046cd11768de7901
Rodp63 <joaquin.palma@ucsp.edu.pe>
2019-06-25 13:26:10 -0500

    fixes

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
-<!DOCTYPE html>
 <html>
+<head>
   <img class="centrado" id="titulo" src="images/bender.gif" alt="Titulo">
   <script src="js/bootstrap.js"></script>
   <link rel="stylesheet" href="css/styles.css">
   <script src="js/jquery-3.3.1.min.js"></script>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no, user-scalable=no">
+</head>
 <body>
   <div id="todo">
     <div id="chat_box"> <br>
+      <form action='index.php' method='post'>
+    	  <div align="center">
+    	    User: <input type="text" name="username">
+    	    Password: <input type="password" name = "password"/>
+          <input type="submit" value="Login!"/>
+    	  </div>
+    	</form>
+      <br>
       Hola, soy Bender, te preguntaras que hace un robot tan genial como yo en una pagina para responder dudas de estudiantes. Bueno, todo comenzo cuando mi novia me dejo, mi vida perdio el sentido y me refugie en la programacion web, llenando asi este triste vacio. Ahora mi conciencia esta en esta pagina para ayudar a novatos como tu a ser mejores desarroladores web cada vez. Por suerte soy un robot que lo sabe todo, asi que si quieres dejar de ser tan malo programando, ven y platiquemos...
       <br>
       <buttom id="boton" class="centrado" onclick="window.location.href = 'bot.php';" > <center> Platica conmigo! </center> </buttom>
